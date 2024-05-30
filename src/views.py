46158a3b38d52a9dbd0b398d3bb69b4cfb8b1c6f
z2go <120998769+z2go@users.noreply.github.com>
2024-05-30 15:15:04 -0400

    Add files via upload

--- src/views.py
+++ src/views.py
@@ -4,38 +4,38 @@ from flask_login import login_required, current_user
 views = Blueprint('views', __name__)
 
 @views.route('/', methods=['GET', 'POST'])
-@login_required
+#@login_required
 def index():
-    return render_template("index.html", user=current_user, userName=current_user.first_name)
+    return render_template("dashboard.html", user=current_user, userName=current_user.first_name)
 
 #TODO THESE PAGES
 @views.route("/dashboard")
-@login_required
+#@login_required
 def dashboard():
     return render_template("dashboard.html")
 
 @views.route("/events")
-@login_required
+#@login_required
 def events():
     return render_template("events.html")
 
 @views.route('/friends', methods=['GET'])
-@login_required
+#@login_required
 def friends():
     return render_template("friends.html", user=current_user)
 
 @views.route('/notifications', methods=['GET'])
-@login_required
+#@login_required
 def notifications():
     return render_template("notifications.html", user=current_user)
 
 @views.route('/explore_jobs', methods=['GET'])
-@login_required
+#@login_required
 def explore_jobs():
     return render_template("explore_jobs.html", user=current_user)
 
 @views.route('/settings', methods=['GET', 'POST'])
-@login_required
+#@login_required
 def settings():
     if request.method == 'POST':
         pass
